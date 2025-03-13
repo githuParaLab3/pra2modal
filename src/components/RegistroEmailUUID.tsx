b85d4ef8d532d8d28f5647e6191316b3e936a6e7
githuParaLab3 <leshenlier@gmail.com>
2025-03-12 21:48:29 -0300

    Segundo Commit

--- src/components/RegistroEmailUUID.tsx
+++ src/components/RegistroEmailUUID.tsx
@@ -2,9 +2,7 @@ import ModalScreen from "@/src/components/ModalScreen";
 import { TextInput } from "react-native-gesture-handler";
 import { Button } from "./Button";
 import { useState } from "react";
-import * as Crypto from "expo-crypto";
 import { ThemedText } from "./ThemedText";
-
 
 type Props = {
     isVisible: boolean;
@@ -12,18 +10,34 @@ type Props = {
 };
 
 export default function RegistroEmailUUID({ isVisible, onClose }: Props) {
-    const [UUIDgerado, setUUID] = useState<String>("");
+    const [email, setEmail] = useState<string>("");
+    const [UUIDgerado, setUUID] = useState<string>("");
 
     const GeradorDeUUID = () => {
-        const uuid = Crypto.randomUUID(); 
-        setUUID(uuid);
+        const generatedUUID = crypto.randomUUID();
+        setUUID(generatedUUID);
     };
+
     return (
         <ModalScreen isVisible={isVisible} onClose={onClose}>
-            <ThemedText>Insira um email:</ThemedText>
-            <TextInput></TextInput>
+            <ThemedText style={{ color: "#fff" }}>Insira um email:</ThemedText>
+            <TextInput
+                value={email}
+                onChangeText={setEmail}
+                placeholder="Digite seu e-mail"
+                placeholderTextColor="#ddd"
+                style={{
+                    color: "#fff",
+                    borderColor: "#ccc",
+                }}
+            />
             <Button label="Gerar UUID" onPress={GeradorDeUUID} />
-            <ThemedText>UUID: {UUIDgerado}</ThemedText>
+            <ThemedText style={{ color: "#fff" }}>
+                E-mail cadastrado: {email}
+            </ThemedText>
+            <ThemedText style={{ color: "#fff" }}>
+                Seu UUID gerado é: {UUIDgerado}
+            </ThemedText>
         </ModalScreen>
     );
-};
+}
